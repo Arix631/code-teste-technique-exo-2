b10bd5da4661dd803e1c4d3b487f7dc1e2b1bafd
Amine <kebaieramine1@gmail.com>
2025-09-05 15:29:02 +0200

    exercice2-test-technique-tims

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
 
 
+function palindrome(chaine1) {
+    for (let i = 0 ; i <= Math.floor((chaine1.length - 1) / 2) ; i++) {
+          if( chaine1[i] != chaine1[chaine1.length-1 -i]){
+             console.log("faux");
+             return;
+          }
+          
+        
+    }
+
+console.log("vrai"); 
+}
+
+
+ palindrome("kayak");
+ palindrome("abba");
+ palindrome("tims");
+ palindrome("abab");
